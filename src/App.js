130efdd293fd476bfb76b45f549e3154b0e9d2cf
Brianna Workman <brianna.workman@hotmail.com>
2020-02-14 23:12:01 -0600

    Create react app and initial cleanup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Brianna Workman</h1>
+      <h2>Software Engineer</h2>
+    </div>
+  );
+}
+
+export default App;
