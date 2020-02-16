7fa38176584c8d1277cb1464ba1e3155b0545d36
Brianna Workman <brianna.workman@hotmail.com>
2020-02-15 19:35:19 -0600

    Adds header and header buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../App.css";
+import "../assets/styles/header.css";
 
 function App() {
   return (
@@ -7,6 +7,17 @@ function App() {
       <p className="header-p">Hi, my name is</p>
       <h1 className="header-h1">Brianna Workman.</h1>
       <h2 className="header-h2">I am a Software Engineer</h2>
+      <div className="button-container">
+        <a href="mailto:brianna.workman@hotmail.com">
+          <button className="button">Get In Touch</button>
+        </a>
+        <a href="https://github.com/briworkman" target="_blank">
+          <button className="button">GitHub</button>
+        </a>
+        <a href="https://resume.creddle.io/resume/ba5qjpkswa6" target="_blank">
+          <button className="button">Resume</button>
+        </a>
+      </div>
     </div>
   );
 }
