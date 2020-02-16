1bdf7316ad4b0106d99ae8c489fd1c6b569e50f3
Brianna Workman <brianna.workman@hotmail.com>
2020-02-15 23:10:22 -0600

    adds navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
 import SideLinks from "./components/SideLinks";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="app">
+      <Nav />
       <Header />
       <SideLinks />
     </div>
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../assets/styles/nav.css";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <div className="container">
+        <a href="#">About</a>
+        <a href="#">Projects</a>
+        <a href="#">Articles</a>
+        <a href="#">Contact</a>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
