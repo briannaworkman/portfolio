b3d3b7bf94f593281e15e1677f58efb1d416033d
Brianna Workman <brianna.workman@hotmail.com>
2020-02-15 18:26:53 -0600

    Sets up file structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Brianna Workman</h1>
-      <h2>Software Engineer</h2>
-    </div>
-  );
+  return <Header />;
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+
+function App() {
+  return (
+    <div className="header">
+      <p className="header-p">Hi, my name is</p>
+      <h1 className="header-h1">Brianna Workman.</h1>
+      <h2 className="header-h2">I am a Software Engineer</h2>
+    </div>
+  );
+}
+
+export default App;
