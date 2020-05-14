bb7becd65cda49d05b06164b01b06f7572977da1
Brianna Workman <brianna.workman@hotmail.com>
2020-05-13 23:28:41 -0400

    adds copy to clipboard and toast for email

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,92 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../assets/styles/nav.css';
 import resume from '../assets/brianna-workman.pdf';
+import styled from 'styled-components';
+
+const Snackbar = styled('div')`
+  min-width: 20vw;
+  background-color: #ffff;
+  color: #000;
+  text-align: center;
+  border-radius: 2px;
+  padding: 20px;
+  position: fixed;
+  z-index: 1;
+  left: 95%;
+  transform: translateX(-100%);
+  bottom: 2vh;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
+  animation: fadein 0.5s, fadeout 0.5s 2.5s;
+  @-webkit-keyframes fadein {
+    from {
+      bottom: 0;
+      opacity: 0;
+    }
+    to {
+      bottom: 2vh;
+      opacity: 1;
+    }
+  }
+  @keyframes fadein {
+    from {
+      bottom: 0;
+      opacity: 0;
+    }
+    to {
+      bottom: 2vh;
+      opacity: 1;
+    }
+  }
+  @-webkit-keyframes fadeout {
+    from {
+      bottom: 2vh;
+      opacity: 1;
+    }
+    to {
+      bottom: 0;
+      opacity: 0;
+    }
+  }
+  @keyframes fadeout {
+    from {
+      bottom: 2vh;
+      opacity: 1;
+    }
+    to {
+      bottom: 0;
+      opacity: 0;
+    }
+  }
+`;
 
 function Nav() {
+  const [email] = useState('brianna-workman@lambdastudents.com');
+  const [showToast, setShowToast] = useState(false);
+  const [toastText, setToastText] = useState('');
+
+  const copyText = (value) => {
+    window.navigator.clipboard.writeText(value);
+    setShowToast(true);
+    setToastText(`${value} copied to the clipboard.`);
+  };
+
+  useEffect(() => {
+    if (showToast) {
+      setTimeout(() => setShowToast(false), 2500);
+    }
+  }, [showToast, setShowToast]);
+
   return (
     <div className='nav'>
       <div className='container'>
-        <a href='mailto:brianna.workman@hotmail.com'>Contact</a>
+        <a onClick={() => copyText(email)}>{email}</a>
         <a href={resume} download>
           Resume
         </a>
       </div>
+      {showToast && <Snackbar>{toastText}</Snackbar>}
     </div>
   );
 }
