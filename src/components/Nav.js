966ca11dda7cffe47f84067e3b8c5f4e7144f219
Brianna Workman <brianna.workman@hotmail.com>
2020-05-21 21:43:19 -0400

    add scrolling navbar and new styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,71 @@
-import React from "react";
-import "../assets/styles/nav.css";
-import resume from "../assets/brianna-workman.pdf";
+import React from 'react';
+import '../assets/styles/nav.css';
+import resume from '../assets/brianna-workman.pdf';
+import { Link } from 'react-scroll';
 
 function Nav() {
   return (
-    <div className="nav">
-      <div className="container">
-        <a href={resume} download>
-          Resume
-        </a>
+    <nav className='nav' id='navbar'>
+      <div className='container'>
+        <ul className='nav-items'>
+          <li className='nav-item'>
+            <Link
+              activeClass='active'
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={1000}
+              to='header'
+              activeClassName='selected'
+            >
+              Home
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link
+              activeClass='active'
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={1000}
+              to='about'
+              activeClassName='selected'
+            >
+              About
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link
+              activeClass='active'
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={1000}
+              to='project'
+              activeClassName='selected'
+            >
+              Projects
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link
+              activeClass='active'
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={1000}
+              to='contact'
+              activeClassName='selected'
+            >
+              Contact
+            </Link>
+          </li>
+          <a href={resume} download>
+            Resume
+          </a>
+        </ul>
       </div>
-    </div>
+    </nav>
   );
 }
 
