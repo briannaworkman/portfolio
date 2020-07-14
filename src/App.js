06c2ecb6d925a283e6d17b8d5daa93f1c9d0f393
Brianna Workman <brianna.workman@hotmail.com>
2020-07-14 15:51:01 -0400

    adds axios

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useEffect } from 'react';
 
 // Component imports
-import Header from "./components/Header";
-import SideLinks from "./components/SideLinks";
-import Nav from "./components/Nav";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+import Header from './components/Header';
+import SideLinks from './components/SideLinks';
+import Nav from './components/Nav';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import axios from 'axios';
 
 function App() {
   return (
-    <div className="app">
+    <div className='app'>
       <Nav />
-      <div className="app-container">
+      <div className='app-container'>
         <Header />
         <About />
         <Projects />
