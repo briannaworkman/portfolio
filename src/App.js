4b8c9c338b0c45a6b255f244799f992cd98fccfb
Brianna Workman <brianna.workman@hotmail.com>
2020-08-22 15:52:41 -0400

    reverts back to old styles but keeps profile image change

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React from 'react';
 // Component imports
 import Header from './components/Header';
 import SideLinks from './components/SideLinks';
-import Nav from './components/Nav';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Nav from './components/Nav';
 
 function App() {
   return (
