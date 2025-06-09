fce03f5e0d5fcb75ae8923c51f88b776de373694
Bri Workman <107228348+briannaworkman@users.noreply.github.com>
2025-06-09 19:37:20 -0400

    Updates resume link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,66 +1,66 @@
-import React from 'react';
-import '../assets/styles/nav.css';
-import resume from '../assets/Brianna_Workman_Resume.pdf';
-import { Link } from 'react-scroll';
+import React from "react";
+import "../assets/styles/nav.css";
+import resume from "../assets/Resume.pdf";
+import { Link } from "react-scroll";
 
 function Nav() {
   return (
-    <nav className='nav' id='navbar'>
-      <div className='container'>
-        <ul className='nav-items'>
-          <li className='nav-item'>
+    <nav className="nav" id="navbar">
+      <div className="container">
+        <ul className="nav-items">
+          <li className="nav-item">
             <Link
-              activeClass='active'
+              activeClass="active"
               spy={true}
               smooth={true}
               offset={-70}
               duration={1000}
-              to='header-container'
-              activeClassName='selected'
+              to="header-container"
+              activeClassName="selected"
             >
               Home
             </Link>
           </li>
-          <li className='nav-item'>
+          <li className="nav-item">
             <Link
-              activeClass='active'
+              activeClass="active"
               spy={true}
               smooth={true}
               offset={-70}
               duration={1000}
-              to='about'
-              activeClassName='selected'
+              to="about"
+              activeClassName="selected"
             >
               About
             </Link>
           </li>
-          <li className='nav-item'>
+          <li className="nav-item">
             <Link
-              activeClass='active'
+              activeClass="active"
               spy={true}
               smooth={true}
               offset={-70}
               duration={1000}
-              to='project'
-              activeClassName='selected'
+              to="project"
+              activeClassName="selected"
             >
               Projects
             </Link>
           </li>
-          <li className='nav-item'>
+          <li className="nav-item">
             <Link
-              activeClass='active'
+              activeClass="active"
               spy={true}
               smooth={true}
               offset={-70}
               duration={1000}
-              to='contact'
-              activeClassName='selected'
+              to="contact"
+              activeClassName="selected"
             >
               Contact
             </Link>
           </li>
-          <a href={resume} download className='nav-item'>
+          <a href={resume} download className="nav-item">
             Resume
           </a>
         </ul>
