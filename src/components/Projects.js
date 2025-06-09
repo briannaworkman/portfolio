435f98cf524521ef7b7902b419c3cd1c32c37e2c
Bri Workman <107228348+briannaworkman@users.noreply.github.com>
2025-06-09 19:23:17 -0400

    Updates key description

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,49 +1,49 @@
-import React from 'react';
-import '../assets/styles/projects.css';
-import github from '../assets/icons/github-brands.svg';
-import launch from '../assets/icons/launch.svg';
-import projects from '../utils/projectInfo';
+import React from "react";
+import "../assets/styles/projects.css";
+import github from "../assets/icons/github-brands.svg";
+import launch from "../assets/icons/launch.svg";
+import projects from "../utils/projectInfo";
 
 function Projects() {
   return (
-    <div className='projects-container'>
-      <div className='project'>
+    <div className="projects-container">
+      <div className="project">
         <h2>My Projects</h2>
       </div>
       {projects.map((projects) => {
         return (
-          <div className='key-project'>
-            {projects.title !== 'Key Conservation' ? (
-              <div className='covid-pic'>
+          <div className="key-project">
+            {projects.title !== "Key Conservation" ? (
+              <div className="covid-pic">
                 {projects.img.map((image) => {
-                  return <img src={image} alt='Application Example' />;
+                  return <img src={image} alt="Application Example" />;
                 })}
               </div>
             ) : (
-              <div className='key-pic' alt='Application Example'>
+              <div className="key-pic" alt="Application Example">
                 {projects.img.map((image) => {
-                  return <img src={image} alt='application example' />;
+                  return <img src={image} alt="application example" />;
                 })}
               </div>
             )}
-            <div className='key-text'>
-              <div className='project-header'>
+            <div className="key-text">
+              <div className="project-header">
                 <h2>{projects.title}</h2>
-                <div className='icons'>
+                <div className="icons">
                   <a
                     href={projects.github_url}
-                    target='_blank'
-                    rel='noopener noreferrer'
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
-                    <img src={github} alt='Go to GitHub' />
+                    <img src={github} alt="Go to GitHub" />
                   </a>
                   {projects.live_url ? (
                     <a
                       href={projects.live_url}
-                      target='_blank'
-                      rel='noopener noreferrer'
+                      target="_blank"
+                      rel="noopener noreferrer"
                     >
-                      <img src={launch} alt='Go Live' />
+                      <img src={launch} alt="Go Live" />
                     </a>
                   ) : null}
                 </div>
@@ -51,21 +51,21 @@ function Projects() {
               <p>{projects.description}</p>
               {projects.live_url === null ? (
                 <p>
-                  Due to COVID-19, the launch of this app has been postponed.
-                  You can view updates on the{' '}
+                  This app is currently undergoing maintenance and is not live.
+                  You can view updates on the{" "}
                   <a
-                    href='https://www.keyconservation.org/'
-                    target='_blank'
-                    rel='noopener noreferrer'
+                    href="https://www.keyconservation.org/"
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
                     Key Conservation
-                  </a>{' '}
+                  </a>{" "}
                   website
                 </p>
               ) : null}
               <p>I built the {projects.role} with:</p>
-              <div className='tech-lists'>
-                <ul className='stack-points'>
+              <div className="tech-lists">
+                <ul className="stack-points">
                   {projects.built_with.map((stack) => {
                     return <li>{stack}</li>;
                   })}
