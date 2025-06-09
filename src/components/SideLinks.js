0e696928b3f51c4d9f32c110f9b4703f3d072969
Bri Workman <107228348+briannaworkman@users.noreply.github.com>
2025-06-09 19:24:04 -0400

    Updates github link

--- src/components/SideLinks.js
+++ src/components/SideLinks.js
@@ -1,42 +1,42 @@
-import React from 'react';
-import '../assets/styles/side-links.css';
-import github from '../assets/icons/github-brands.svg';
-import linkedin from '../assets/icons/linkedin-brands.svg';
-import medium from '../assets/icons/medium-brands.svg';
+import React from "react";
+import "../assets/styles/side-links.css";
+import github from "../assets/icons/github-brands.svg";
+import linkedin from "../assets/icons/linkedin-brands.svg";
+import medium from "../assets/icons/medium-brands.svg";
 
 function SideLinks() {
   return (
-    <div className='side-links'>
+    <div className="side-links">
       <ul>
         <li>
           <a
-            href='https://github.com/briworkman'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://github.com/briannaworkman"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <img src={github} alt="Brianna Workman's GitHub" />
           </a>
         </li>
         <li>
           <a
-            href='https://www.linkedin.com/in/brianna-m-workman/'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://www.linkedin.com/in/brianna-m-workman/"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <img src={linkedin} alt="Brianna Workman's LinkedIn" />
           </a>
         </li>
         <li>
           <a
-            href='https://medium.com/@briworkman9'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://medium.com/@briworkman9"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <img src={medium} alt="Brianna Workman's Medium Profile" />
           </a>
         </li>
       </ul>
-      <div className='after'></div>
+      <div className="after"></div>
     </div>
   );
 }
