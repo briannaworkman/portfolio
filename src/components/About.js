70a463ae40cb40f20f55e2c86f81a697fe31e420
Bri Workman <107228348+briannaworkman@users.noreply.github.com>
2025-06-09 19:29:40 -0400

    Updates about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import profile from '../assets/images/profile.png';
-import '../assets/styles/about.css';
+import React from "react";
+import profile from "../assets/images/profile.png";
+import "../assets/styles/about.css";
 
 export default function About() {
   return (
-    <div className='main-about-container'>
-      <div className='about-container'>
-        <div className='about'>
+    <div className="main-about-container">
+      <div className="about-container">
+        <div className="about">
           <h2>About Me</h2>
           <p>
-            Hello! I'm Brianna, a software engineer who enjoys building
-            exceptional websites and apps that provide intuitive, responsive,
-            pixel-perfect user interfaces with efficient and modern backends.
+            Hello! I'm Brianna, a software engineer with over 5 years of
+            experience. I enjoy building exceptional websites and apps that
+            provide intuitive, responsive, pixel-perfect user interfaces with
+            efficient and modern backends.
           </p>
           <p>
-            I recently completed Lambda School's Full Stack Web Development
-            program where I worked on a wide variety of fun and interesting
-            projects on a daily basis.
+            I'm currently working at Nerdery where I get to implement a wide
+            range of features and collaborate across domains daily.
           </p>
           <p>Here are a few technologies I've been working with recently:</p>
-          <div className='tech-lists'>
+          <div className="tech-lists">
             <ul>
-              <li>JavaScript(ES6+)</li>
-              <li>React & React Native</li>
-              <li>Node & Express</li>
+              <li>TypeScript</li>
+              <li>Next.js</li>
+              <li>React Hook Form</li>
             </ul>
             <ul>
-              <li>HTML & CSS</li>
-              <li>Redux & Context</li>
+              <li>Vitext, Jest, & React Testing Library</li>
+              <li>Redux Toolkit</li>
               <li>Python & Django</li>
             </ul>
           </div>
         </div>
-        <div className='profile-picture'>
-          <img src={profile} alt='Brianna Workman' />
+        <div className="profile-picture">
+          <img src={profile} alt="Brianna Workman" />
         </div>
       </div>
     </div>
